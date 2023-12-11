c5b60343cb932321317854000f01672c0ed95351
samjacobsen13 <143229760+samjacobsen13@users.noreply.github.com>
2023-12-11 13:07:54 -0700

    product filter fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ require 'net/http'
 class ProductsController < ApplicationController
   def index
     if params[:search].present?
-      @display_products = Product.filter_by_title(params[:search])
+      @display_products = fetch_products_from_api(params[:search])
     else
       @display_products = fetch_products_from_api
     end
@@ -12,9 +12,15 @@ class ProductsController < ApplicationController
 
   private
 
-  def fetch_products_from_api
-    uri = URI('https://fakestoreapi.com/products')
-    response = Net::HTTP.get(uri)
-    JSON.parse(response)
+  def fetch_products_from_api(search = nil)
+    url = URI.parse('https://fakestoreapi.com/products')
+    response = Net::HTTP.get_response(url)
+    products = JSON.parse(response.body)
+
+    if search
+      products.select { |product| product['title'].downcase.include?(search.downcase) }
+    else
+      products
+    end
   end
 end
